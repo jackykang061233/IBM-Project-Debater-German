955e85d7904e028da1c8bbe86d2bdbe5bff8c073
jackykang061233 <43503345+jackykang061233@users.noreply.github.com>
2021-10-31 12:47:32 +0100

    with documentation 2021/10/31

--- Stanza_Pattern.py
+++ Stanza_Pattern.py
@@ -1,27 +1,99 @@
-import pandas as pd
-pd.set_option('display.max_columns', 10)
+# spacy
 import spacy
+# stanza
 import stanza
 from stanza.server import CoreNLPClient
+# basic functions
+import pandas as pd
+pd.set_option('display.max_columns', 10)  # show at most 10 columns
+
 
 class Stanza_Pattern:
-    #'([lemma: sein]?) /eine/ /form/ /des|der|von|vom/ '
-    #' /,?/ /oder/ /andere/ /Arten/ /des|der|von|vom/'
+    """
+    A class used to represent an Animal
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+    sent_segmentation(text):
+        This method performs the sentence segmentation of a text with help of spacy
+    tokenization(sentence):
+        This method returns result of the tokenization of a sentence with help of spacy
+    stanza_processor_sentence(sentence):
+        This method returns result of the several NLP tasks on each token of a sentence with help of stanza
+    pattern_extraction_text(text, dc, pattern):
+        1. Do the sentence segmentation of the text
+        2. Add the dc to the regex pattern
+        3. Extract the pattern from each sentence
+        4. Loop through all the matches and get the EC and the whole matched sentences
+    pattern_extraction_sentence(sentence, pattern):
+        This method uses the token-level regular expression to match our desired patterns
+    pattern_dc_construction(dc, pattern):
+        This method uses the token-level regular expression to match our desired patterns
+    pattern_dc_construction(dc, pattern):
+        This method returns the string of the concatenation of dc and pattern
+    process(text):
+        This method performs the pattern matching process
+    """
+    # '([lemma: sein]?) /eine/ /form/ /des|der|von|vom/ '
+    # ' /,?/ /oder/ /andere/ /Arten/ /des|der|von|vom/'
     def __init__(self):
+        """
+        Parameters
+        ----------
+        client:
+            a Stanford CoreNLP server of the german language
+        pattern:
+            all to match patterns
+        nlp_spacy:
+            the spacy german model
+        nlp_stanza:
+            the stanza german pipeline
+        """
         self.client = CoreNLPClient(properties='german', annotators=['tokenize', 'ssplit', 'pos', 'lemma', 'ner', 'parse'], timeout=30000, memory='16G')
         self.pattern = ['/ist/ /ein|eine/', '/ist/ /ein/ /Beispiel/ /für/', '/ist/ /eine/ /Form/ /des|der|von|vom/', '/oder/ /andere/ /Arten/ /des|der|von|vom/', '/oder/ /eine/ /andere/ /Art/ /des|der|von|vom/']
         self.nlp_spacy = spacy.load("de_core_news_sm")
         self.nlp_stanza = stanza.Pipeline(lang='de', processors='tokenize, mwt, lemma, pos, depparse')
 
     def sent_segmentation(self, text):
+        """
+        This method performs the sentence segmentation of a text with help of spacy
+
+        Parameters
+        ----------
+        text: String
+            the string of a text
+
+        Returns
+        ------
+        List:
+            a list contains of the result of the sentence segmentation of a text
+
+        """
         sentences = []
         doc = self.nlp_spacy(text)
         assert doc.has_annotation("SENT_START")
         for sent in doc.sents:
-            sentences.append(sent.text)
+            sentences.append(sent.text)  # append sentence to list
         return sentences
 
     def tokenization(self, sentence):
+        """
+        This method returns result of the tokenization of a sentence with help of spacy
+
+        Parameters
+        ----------
+        sentence: String
+            the string of a sentence
+
+        Returns
+        ------
+        List:
+            a list contains of the result of the tokenization of a sentence
+
+        """
         doc = self.nlp_spacy(sentence)
 
         return [token.text for token in doc]
@@ -29,7 +101,21 @@ class Stanza_Pattern:
     def dependency_parser_text(self, text):
         doc = self.nlp_spacy(text)
 
-    def dependency_parser_sentence(self, sentence):
+    def stanza_processor_sentence(self, sentence):
+        """
+        This method returns result of the several NLP tasks on each token of a sentence with help of stanza
+
+        Parameters
+        ----------
+        sentence: String
+           the string of a sentence
+
+        Returns
+        ------
+        List:
+           a list contains of the result of stanza pipeline with processors: tokenizer, multi-word tokenizer, lemmatizer, pos tagger, dependency parser
+
+       """
         doc = self.nlp_stanza(sentence)
 
         return doc.sentences
@@ -51,7 +137,7 @@ class Stanza_Pattern:
         Parameters
         ----------
         text: String
-            a string of text
+            the string of text
         dc: String
             debate concept
         pattern: String
@@ -69,7 +155,7 @@ class Stanza_Pattern:
         list_rows = []  # store the every pair of (dc, ec, whole sentence)
         for sent in sentences:
             match = self.pattern_extraction_sentence(sent, pattern_dc)  # find the match from the pattern
-            dependency_parse = self.dependency_parser_sentence(sent)  # the dependency parser of the sentence
+            dependency_parse = self.stanza_processor_sentence(sent)  # the dependency parser of the sentence
 
             for index in range(match["sentences"][0]["length"]):
                 begin = match['sentences'][0][str(index)]["begin"]  # get the beginning index of each matched sentence
@@ -83,15 +169,47 @@ class Stanza_Pattern:
 
                 dict_row = {'DC': dc, 'EC': ec, 'Whole Sentence': whole_sentence}  # put in the dict
                 list_rows.append(dict_row)
-                print(dependency_parse)
+                # print(dependency_parse)
 
         return list_rows
 
     def pattern_extraction_sentence(self, sentence, pattern):
+        """
+        This method uses the token-level regular expression to match our desired patterns
+
+        Parameters
+        ----------
+        sentence: String
+            the string of a sentence
+        pattern: String
+            to match pattern
+
+        Returns
+        ------
+        Dict:
+            a dictionary contains of the matched patterns in a sentence
+       """
         matches = self.client.tokensregex(sentence, pattern)
+
         return matches
 
     def pattern_dc_construction(self, dc, pattern):
+        """
+        This method returns the string of the concatenation of dc and pattern
+
+        Parameters
+        ----------
+        dc: String
+            debate concept
+        pattern: String
+            to match pattern
+
+        Returns
+        ------
+        String
+            a string contains of the concatenation of dc and pattern
+
+        """
         tokens = self.tokenization(dc)
         pattern_dc = "/ /".join(tokens)  # add the dc to the regex
         pattern_dc = '/' + tokens[0].lower() + '|' + pattern_dc + '/ ' + pattern  # dc can be the first word
@@ -99,7 +217,20 @@ class Stanza_Pattern:
         return pattern_dc
 
     def process(self, text):
-        # text = text.lower()
+        """
+        This method performs the pattern matching process
+
+        Parameters
+        ----------
+        text: String
+           the string of a text
+
+        Returns
+        ------
+        Dataframe:
+           a list contains of all the matched pairs of DC and EC in the form of DC, EC, Whole Sentence
+
+       """
         list_rows = []
         for pattern in self.pattern:
             list_rows += self.pattern_extraction_text(text, 'Das', pattern)
